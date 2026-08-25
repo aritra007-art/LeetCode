5b75393297ab55e0bc43680389ef9638f794642c
Aritra Pal <aritra345pal@gmail.com>
2026-08-26 01:02:02 +0530

    Time: 1 ms (82.00%) | Memory: 44.7 MB (95.40%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        boolean[] seen = new boolean[101];
+        for (int x : nums) {
+            if (x < seen.length) {
+                seen[x] = true;
+            }
+        }
+        for (int i = 1; ; ++i) {
+            int x = k * i;
+            if (x >= seen.length || !seen[x]) {
+                return x;
+            }
+        }
+    }
+}
